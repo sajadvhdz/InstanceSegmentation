0dd81647e8e33a33edcb2c48a2b7f6b174de2fd9
vahed005@umn.edu <Eminem4ever!>
2022-06-22 13:24:16 -0500

    Third Round of Files

--- Detector.py
+++ Detector.py
@@ -14,8 +14,8 @@ class Detector:
         self.cfg = get_cfg()
 
         # Load model config and pretrained model
-        self.cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml"))
-        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_101_FPN_3x.yaml")
+        self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+        self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
 
         self.cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7
         self.cfg.MODEL.DEVICE = "cuda"
@@ -23,13 +23,32 @@ class Detector:
         self.predictor = DefaultPredictor(self.cfg)
 
     def onImage(self, imagePath):
-            image = cv2.imread(imagePath)
+        image = cv2.imread(imagePath)
+        predictions = self.predictor(image)
+
+        viz = Visualizer(image[:, :, ::-1], metadata=MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]), instance_mode= ColorMode.SEGMENTATION)
+        output = viz.draw_instance_predictions(predictions["instances"].to("cpu"))
+
+        plt.imshow(output.get_image()[:, :, ::-1])
+        plt.show()
+
+    def onVideo(self, videoPath):
+        cap = cv2.VideoCapture(videoPath)
+
+        if (cap.isOpened()==False):
+            print("Error opening the file...")
+            return
+
+        (success, image)= cap.read()
+
+        while success:
             predictions = self.predictor(image)
 
-            viz = Visualizer(image[:,:,::-1], metadata = MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]),
-                             instance_mode=ColorMode.IMAGE)
+            viz = Visualizer(image[:, :, ::-1], metadata=MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]),
+                             instance_mode=ColorMode.SEGMENTATION)
             output = viz.draw_instance_predictions(predictions["instances"].to("cpu"))
 
-            plt.imshow("Results", output.get_image())
+            plt.imshow(output.get_image()[:, :, ::-1])
             plt.show()
-
+            plt.waitforbuttonpress(.001)
+            (success, image) = cap.read()
--- main.py
+++ main.py
@@ -2,5 +2,6 @@ from Detector import *
 
 detector = Detector()
 detector.onImage("1.jpg")
+detector.onVideo("street.mp4")
 
 print("Done.")
