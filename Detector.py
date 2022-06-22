6b9acfa19b5cef3ec8756aaf334a6db5302a4b24
vahed005@umn.edu <Eminem4ever!>
2022-06-22 09:58:06 -0500

    First sample

--- Detector.py
+++ Detector.py
@@ -1,1 +1,18 @@
 from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.data import MetadataCatalog
+from detectron2.utils.visualizer import ColorMode, visualizer
+from detectron2 import model_zoo
+
+import cv2
+import numpy as np
+
+class Detector:
+    def __init__(self):
+        self.cfg = get_cfg()
+
+        # Load model config and pretrained model
+        self.cfg.merge_from_file(model_zoo.get_config_file("CODO"))
+
+
+print("Hello")
