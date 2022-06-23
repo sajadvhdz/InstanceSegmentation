f1371df24949f7340d1f3c7805c9c554dad6f65a
vahed005@umn.edu <Eminem4ever!>
2022-06-23 09:33:44 -0500

    Third Round of Files

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from Detector import *
 detector = Detector()
 detector.onImage("1.jpg")
 detector.onImage("2.png")
-detector.onVideo("street.mp4")
+# detector.onVideo("street.mp4")
 
 print("Done.")
