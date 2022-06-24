b1324f13485f2a5778b6561eb5dd9755ce09dfa9
vahed005@umn.edu <Eminem4ever!>
2022-06-24 14:28:58 -0500

    Sixth Round of Files

--- main.py
+++ main.py
@@ -5,23 +5,9 @@ detector = Detector()
 
 '''
 Merge two json files as follows:
-# python merge.py result1.json result2.json OUTPUT_JSON.json
-merge.py is stored in the IS folder.
+# python merge_prime.py result1.json result2.json train_dataset.json
+merge_prime.py is stored in the IS folder.
 '''
-
-'''
-Correct the filepath for all images and save the new json file as 'train_dataset'
-'''
-import json
-with open("OUTPUT_JSON.json") as f:
-    dict = json.load(f)
-    for i, j in enumerate(dict['images']):
-        str_x = dict['images'][i]['file_name']
-        dict['images'][i]['file_name'] = str_x.replace('images\\7/', '')
-
-output_file = "train_dataset"
-with open(output_file, 'w') as f:
-        json.dump(dict, f)
 
 #%%
 
