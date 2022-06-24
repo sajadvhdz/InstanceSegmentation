d129d29bf4862937ddbfbf5107e9f28bbe60e3ee
vahed005@umn.edu <Eminem4ever!>
2022-06-24 13:53:37 -0500

    Fifth Round of Files

--- Detector.py
+++ Detector.py
@@ -6,13 +6,11 @@ from detectron2 import model_zoo
 
 import cv2
 from matplotlib import pyplot as plt
-import keyboard
 
 
 class Detector:
     def __init__(self):
         self.cfg = get_cfg()
-
         # Load model config and pretrained model
         self.cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
         self.cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")
@@ -31,33 +29,3 @@ class Detector:
 
         plt.imshow(output.get_image()[:, :, ::-1])
         plt.show()
-
-    def onVideo(self):
-        cap = cv2.VideoCapture(0)
-
-        # cap = cv2.VideoCapture(videoPath)
-
-        if (cap.isOpened()==False):
-            print("Error opening the file...")
-            return
-
-        (success, image)= cap.read()
-
-        while success:
-            predictions = self.predictor(image)
-
-            viz = Visualizer(image[:, :, ::-1], metadata=MetadataCatalog.get(self.cfg.DATASETS.TRAIN[0]),
-                             instance_mode=ColorMode.SEGMENTATION)
-            output = viz.draw_instance_predictions(predictions["instances"].to("cpu"))
-
-            plt.imshow(output.get_image()[:, :, ::-1])
-            plt.show()
-            plt.waitforbuttonpress(.001)
-            (success, image) = cap.read()
-
-    def pause():
-        while True:
-            if keyboard.read_key() == 'space':
-                # If you put 'space' key
-                # the program will resume.
-                break
--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 from Detector import *
-
 detector = Detector()
 
+#%%
 
+'''
+Merge two json files as follows:
+# python merge.py result1.json result2.json OUTPUT_JSON.json
+merge.py is stored in the IS folder.
+'''
 
+'''
+Correct the filepath for all images and save the new json file as 'train_dataset'
+'''
+import json
+with open("OUTPUT_JSON.json") as f:
+    dict = json.load(f)
+    for i, j in enumerate(dict['images']):
+        str_x = dict['images'][i]['file_name']
+        dict['images'][i]['file_name'] = str_x.replace('images\\7/', '')
+
+output_file = "train_dataset"
+with open(output_file, 'w') as f:
+        json.dump(dict, f)
+
+#%%
+
+import time
 import glob, os
-os.chdir("new_images/")
-for file in glob.glob("*.jpg"):
-    print(file)
-    detector.onImage(file)
-    pause()
-
-# detector.onImage("1.jpg")
-# detector.onImage("2.png")
-# detector.onVideo()
-
-print("Done.")
+# os.chdir("new_images/")
+# for file in glob.glob("*.jpg"):
+#     print(file)
+#     detector.onImage(file)
+#     time.sleep(3)
